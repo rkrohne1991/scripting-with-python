9b35629b4d279c35e9c0c05101d30d7196368f68
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2020-09-15 18:13:14 +0100

    Hiding tweeter secret keys

--- twitter-bot/tweettweet.py
+++ twitter-bot/tweettweet.py
@@ -2,10 +2,10 @@ import tweepy
 import time
 
 # Consumer Keys & Authentication Tokens for TwitterBot Project
-consumer_key = "makyXADeWqm5J19uX7umY1T7o"
-consumer_secret = "PAlNlbQL6nxdWz20yOBTOQSHHbahPdXD6gRaD6tJfTCnpudd8m"
-access_token = "1305847114283573250-kJEYumaAdDTokK2vQ5pjie9GxCWJWf"
-access_token_secret = "TFdUzgpXrchqRAxhsjUZzcmhcCWYSY4NHlNuGERPrdX7u"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -35,7 +35,7 @@ for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
 #Generous bot
 # for follower in limit_handler(tweepy.Cursor(api.followers).items()):
 #     # name of person to find in your followers
-#     follower_name_to_follow = "Joanna Wojcik"
+#     follower_name_to_follow = ""
 #     if follower.name == follower_name_to_follow:
 #         follower.follow()
 #         break
